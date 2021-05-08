604b447f6521daa89271a6c6fd2e8200809d5e31
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-05-09 02:17:57 +0400

    add Car repo

---
+++ src/main/java/org/example/repository/CarRepo.java
@@ -0,0 +1,22 @@
+package org.example.repository;
+
+import org.example.entity.Client;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CarRepo {
+    //create
+    void add(Client client) throws SQLException;
+
+    //read
+    List<Client> getAll() throws SQLException;
+
+    Client getById(Long id) throws SQLException;
+
+    //update
+    Client update(Client client) throws SQLException;
+
+    //delete
+    void remove(Long id) throws SQLException;
+}
